d92592a96a9bd66afea7055bd9d01810431d3829
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-09-14 08:45:13 +0200

    first working links and paragraphs

---
+++ vue/src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>hello michal</h1>
+  </div>
+</template>
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>spacer</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+export default {
+  name: 'homeView',
+};
+</script>
