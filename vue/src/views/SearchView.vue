d61e7055d56851ad9ef5ab9cc7b84a59f865ccf6
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-09-14 09:35:24 +0200

    I finally got everything working

--- vue/src/views/SearchView.vue
+++ vue/src/views/SearchView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <h1>spacer</h1>
+  <div class="search">
+    <h1>Search</h1>
   </div>
 </template>
 
@@ -8,6 +8,6 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
 export default {
-  name: 'homeView',
+  name: 'SearchView',
 };
 </script>
