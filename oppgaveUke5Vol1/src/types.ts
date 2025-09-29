67601056fe8f32140341921a4fccfd6f96d5baf5
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-09-29 10:47:32 +0200

    Type.ts is readey to use...

--- oppgaveUke5Vol1/src/types.ts
+++ oppgaveUke5Vol1/src/types.ts
@@ -1,11 +1,8 @@
 type AppState = {
-    currentId: null,
+    currentId: null | number;
     filesAndFolders: [
-        {id: 1, name: 'Handlelister'},
-        {id: 2, name: 'Ting som skal fikses'},
-        {id: 3, name: 'Oktober', parentId: 1},
-        {id: 4, name: 'Tirsdag 15.', parentId: 3, content: 'melk\nbrød\nost\n'},
-        {id: 5, name: 'Bad', parentId: 2, content: 'Lekkasje, bla bla'},
-        {id: 6, name: 'notater.txt', content: 'abc'},
+        {id: number , name: string, parentId: null | number, content: null | string},
+       
     ],
 };
+export type { AppState };
