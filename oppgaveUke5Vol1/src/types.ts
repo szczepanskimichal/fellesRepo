d5e8e19e713c32f700827fe6fe3329b91c8f894a
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-09-29 12:23:29 +0200

    currentId goes to null and it's fine

--- oppgaveUke5Vol1/src/types.ts
+++ oppgaveUke5Vol1/src/types.ts
@@ -1,5 +1,9 @@
 type AppState = {
     currentId: null | number;
-    filesAndFolders: {id: number , name: string, parentId?: null | number, content?: null | string}[],
+    filesAndFolders: {id: number ,
+        name: string,
+        parentId?: null | number,
+        content?: null | string
+    }[],
 };
 export type { AppState };
--- oppgaveUke5Vol1/src/views/MyApp.ts
+++ oppgaveUke5Vol1/src/views/MyApp.ts
@@ -2,40 +2,32 @@ import { BaseComponent } from "../components/BaseComponent";
 import type { AppState } from "../types";
 
 export class MyApp extends BaseComponent {
-    // static propNames = [];
-    // state: AppState ={
-    //     filesAndFolders: [
-    //     {id: 1, name: 'Handlelister', parentId: null, content: null},
-    //     {id: 2, name: 'Ting som skal fikses', parentId: null, content: null},
-    //     {id: 3, name: 'Oktober', parentId: 1, content: null},
-    //     {id: 4, name: 'Tirsdag 15.', parentId: 3, content: 'melk\nbrød\nost\n'},
-    //     {id: 5, name: 'Bad', parentId: 2, content: 'Lekkasje, bla bla'},
-    //     {id: 6, name: 'notater.txt', parentId: null, content: 'abc'},
-    //     ]
-    // }
+    static propNames: string[] = [];
+
     state: AppState = {
         currentId: null,
         filesAndFolders: [
-            {id: 1, name: 'Handlelister'},
-            {id: 2, name: 'Ting som skal fikses'},
-            {id: 3, name: 'Oktober', parentId: 1, content: null},
+            {id: 1, name: 'Ting som skal fikses'},
+            {id: 2, name: 'Handlelister'},
+            {id: 3, name: 'Oktober', parentId: 2, content: null},
             {id: 4, name: 'Tirsdag 15.', parentId: 3, content: 'melk\nbrød\nost\n'},
-            {id: 5, name: 'Bad', parentId: 2, content: 'Lekkasje, bla bla'},
-            {id: 6, name: 'notater.txt', parentId: null, content: 'abc'},
+            {id: 5, name: 'Bad', parentId: 1, content: 'Lekkasje, bla bla'},
+            {id: 6, name: 'notater.txt', content: 'abc'},
         ]
     }
     render() {
         this.shadowRoot!.innerHTML = `
             <fieldset>
             <legend>Filer & mapper</legend>
-            <p>Du er her{this.state.currentId}</p>
-            <ul>
+            <p>Du er her ${this.state.currentId}</p>
+            <ol>
                 ${this.state.filesAndFolders.map(item => `
                     <li>
                         ${item.name} ${item.parentId ? `(i mappe ${item.parentId})` : ''}
                     </li>
                 `).join('')}
-            </ul>
+            </ol>
+            //vise komponent here!!!
             </fieldset>
 
             
