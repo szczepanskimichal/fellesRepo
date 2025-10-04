3039e0788751b8af74b7cbef157fd0a41c7b8aad
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-10-04 13:57:32 +0200

    add checbox, refactor task management to use objects for tasks with name and done properties

--- ToDoVol-X/src/main.ts
+++ ToDoVol-X/src/main.ts
@@ -2,19 +2,46 @@ const taskNameInputElement: HTMLInputElement = document.querySelector('#name')!;
 const addButtonElement: HTMLButtonElement = document.querySelector('button')!;
 const taskContainerElement: HTMLElement = document.querySelector('.tasks')!;
 
-const tasks: string[] = ["Kast soppel", "trene", "lage mat", "vaske klær", "lage matpakke", "handle mat"];
+
+const tasks: {
+  name: string;
+  done: boolean;
+}[] = [
+  { name: "Kast soppel", done: false },
+  { name: "trene", done: true },
+  { name: "lage mat", done: true },
+  { name: "vaske klær", done: false },
+  { name: "lage matpakke", done: false },
+  { name: "handle mat", done: false }
+];
 
 const render = () => {
-  taskContainerElement.innerHTML = ''; // Clear existing tasks
-  tasks.forEach((task) => { // Iterate over each task
-    const taskElement: HTMLElement = document.createElement('li'); // Create a new list item
-    taskElement.textContent = task; // Set the text content to the task
-    taskContainerElement.appendChild(taskElement); // Append the list item to the task container
+  taskContainerElement.innerHTML = '';
+  tasks.forEach((task, index) => { 
+    const taskElement:HTMLElement = document.createElement('li');
+    const id: string = `task-${index}`;
+    const labelElement:HTMLElement = document.createElement('label');
+    labelElement.textContent = task.name;
+    labelElement.setAttribute('for', id);
+    const checboxElement: HTMLInputElement = document.createElement('input');
+    checboxElement.type = 'checkbox';
+    checboxElement.name = task.name;
+    checboxElement.id = id;
+    //te 2 linijki ponizej ustawia checkbox jako zaznaczony
+    //  jezeli task.done jest true i odpowiada za poprawne odznaczanie/zaznaczanie
+    checboxElement.checked = task.done;
+    checboxElement.addEventListener('change', (event: Event) => {
+      task.done = !task.done;
+    })
+    // tworze i wstrzykuje elementy!!!!!!!!!!
+    taskElement.appendChild(checboxElement); 
+    taskElement.appendChild(labelElement);
+    taskContainerElement.appendChild(taskElement);
   });
 };
 
-const addTask =(task:string) => {
-  tasks.push(task);
+const addTask =(taskName:string) => {
+  tasks.push({ name: taskName, done: false }); // Add the new task to the tasks array
 }
 
 addButtonElement.addEventListener('click', (event: Event) => {
