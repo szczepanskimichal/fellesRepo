b326bfaa045c91c1f86b8b929dcb3ba7bd794cb8
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-11-02 17:57:17 +0100

    working

--- frameworkless/eventbusMyExample/src/buttonModule.ts
+++ frameworkless/eventbusMyExample/src/buttonModule.ts
@@ -7,19 +7,19 @@ export function setupButtons() {
   const colorBtn = document.querySelector<HTMLButtonElement>('#colorBtn')!;
 
   notifyBtn.addEventListener('click', () => {
-    console.log('🟡 Kliknięto przycisk WYŚLIJ');
+    console.log('🟡 clicked send message button');
     // ✅ TypeScript sprawdzi że przekazujesz { message: string }
     eventBus.emit('buttonClicked', { message: 'Cześć z Event Busa 🚀' });
   });
 
   resetBtn.addEventListener('click', () => {
-    console.log('🔵 Kliknięto przycisk RESET');
+    console.log('🔵 clicked reset button');
     // ✅ TypeScript wie że resetScreen nie wymaga danych
     eventBus.emit('resetScreen', undefined);
   });
 
   colorBtn.addEventListener('click', () => {
-    console.log('🟠 Kliknięto przycisk ZMIEŃ KOLOR');
+    console.log('🟠 clicked change color button');
     const colors = ['darkblue', 'red', 'green', 'purple', 'orange', 'brown'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     // ✅ TypeScript sprawdzi że przekazujesz { color: string }
