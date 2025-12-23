fdbf87a68291a87a642adaf8fe38a320568d72b9
Michal Szczepanski <127307935+szczepanskimichal@users.noreply.github.com>
2025-12-23 09:25:59 +0100

    cd with vue and nuxt environment

---
+++ vue/copmositionApi/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="product-displaty">
+    <div class="product-container">
+      <div class="product-info">
+        <h1>Socks</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- vueVol2/revision/src/components/PassingArgumentsWithMetods.vue
+++ vueVol2/revision/src/components/PassingArgumentsWithMetods.vue
@@ -1,49 +1,50 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-  
-  const count = ref(0);
-  const showText = ref(false);
-  const message = ref('');
+import { reactive } from 'vue';
 
-  function addOne(num:number){
-    count.value += num;
-    message.value = ''
-  }
-  function removeOne(num:number){
-    count.value += num;
-  message.value = ''
-  }
-  function addFive(num:number){
-    count.value += num;
-    showText.value=true;
-    message.value = ''
-  }
-  function reset(num:number){
-    count.value = num;
-    showText.value=false;
-    message.value='reset done';
-    setTimeout(() => {
-      message.value='';
-    }, 2000);
-  }
+const state = reactive({
+  count: 0,
+  showText: false,
+  message: ''
+});
 
-
+function addOne(num: number) {
+  state.count += num;
+  state.message = '';
+}
+function removeOne(num: number) {
+  state.count += num;
+  state.message = '';
+}
+function addFive(num: number) {
+  state.count += num;
+  state.showText = true;
+  state.message = '';
+}
+function reset(num: number) {
+  state.count = num;
+  state.showText = false;
+  state.message = 'reset done';
+  setTimeout(() => {
+    state.message = '';
+  }, 2000);
+}
 </script>
+
 <template>
   <div class="wrapper">
-    <p v-if="showText">something other here</p>
+    <p v-if="state.showText">something other here</p>
     <img v-else src="" alt="picture"/>
-    <p>{{ "passing arguments with methods:" + count }}</p>
+    <p>{{ "passing arguments with methods:" + state.count }}</p>
     <button @click="addOne(1)">+1</button>
     <button @click="addFive(5)">+5</button>
     <button @click="removeOne(-1)">-1</button>
     <button @click="reset(0)">Reset</button>
-    <p v-if="count === 0">{{message}}</p>
+    <p v-if="state.count === 0">{{ state.message }}</p>
   </div>
 </template>
-<style scoped>
 
-  .wrapper {
+<style scoped>
+.wrapper {
   border: dashed black 1px;
   width: 250px;
   padding: 10px;
